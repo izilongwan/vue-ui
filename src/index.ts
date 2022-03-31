a6eff3e1e43203fad985d8b55871a82635379504
izilongwan <js.nb@qq.com>
2022-04-01 00:52:58 +0800

    fix: src作为入口，修正types路径

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import './components'
+import UI from './components'
 import App from './index.vue'
+
+export * from './components'
 
 // 热更新
 module?.hot?.accept()
@@ -9,3 +11,5 @@ new Vue({
   el: '#app',
   render: h => h(App),
 })
+
+export default UI
